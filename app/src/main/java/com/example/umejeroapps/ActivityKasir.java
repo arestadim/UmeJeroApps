b31a736cf965db7a0427c5167504793900ca3977
Arsyireyhan <arsyireyhan45@gmail.com>
2021-11-23 08:53:23 +0800

    layout kasir + cardview kasir

---
+++ app/src/main/java/com/example/umejeroapps/ActivityKasir.java
@@ -0,0 +1,14 @@
+package com.example.umejeroapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityKasir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kasir);
+    }
+}
